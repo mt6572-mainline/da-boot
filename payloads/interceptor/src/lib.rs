15acd304f416fc9176c89132f40beb9d8b87874e
rva3 <166127285+rva3@users.noreply.github.com>
2025-11-27 22:50:01 +0200

    interceptor: Add trampoline and code relocator

--- payloads/interceptor/src/lib.rs
+++ payloads/interceptor/src/lib.rs
@@ -1,15 +1,37 @@
 #![no_std]
+#![allow(static_mut_refs)]
+extern crate alloc;
 
-use core::ptr;
+use core::{alloc::Layout, mem::MaybeUninit, ptr};
 
+use alloc::vec::Vec;
 use shared::flush_cache;
 
-use crate::{code::JUMP, err::Error};
+use crate::{
+    code::{JUMP, NOP, extract_ldr, is_32bit, is_ldr, pack_mov_pair, write_thumb2_instr},
+    err::Error,
+};
 
 mod code;
 pub mod err;
 
 pub type Result<T> = core::result::Result<T, Error>;
+
+struct Static<T>(MaybeUninit<T>);
+
+impl<T> Static<T> {
+    unsafe fn init(&mut self, t: T) {
+        self.0.write(t);
+    }
+
+    unsafe fn get(&self) -> &T {
+        unsafe { &*self.0.as_ptr() }
+    }
+
+    unsafe fn get_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.0.as_mut_ptr() }
+    }
+}
 
 #[macro_export]
 macro_rules! hook {
@@ -18,6 +40,9 @@ macro_rules! hook {
     ) => {
         mod $name {
             use super::*;
+
+            #[allow(static_mut_refs)]
+            static mut ADDR: usize = 0;
 
             #[unsafe(naked)]
             #[unsafe(no_mangle)]
@@ -36,28 +61,176 @@ macro_rules! hook {
             }
 
             pub unsafe fn replace(target: usize) -> interceptor::Result<()> {
-                unsafe { Interceptor::replace(target, thunk) }
+                unsafe {
+                    Interceptor::replace(target, thunk)?;
+                    ADDR = target;
+                }
+                Ok(())
+            }
+
+            pub unsafe fn revert() -> interceptor::Result<()> {
+                unsafe {
+                    Interceptor::revert(ADDR)?;
+                    ADDR = 0;
+                }
+                Ok(())
+            }
+
+            pub unsafe fn original() -> *mut u8 {
+                unsafe { Interceptor::original(ADDR) }.unwrap_or(ADDR) as *mut u8
             }
         }
     };
 }
 
+struct Trampoline {
+    address: u32,
+    jump_address: u32,
+}
+
+struct InterceptorPool {
+    address: Vec<u32>,
+    trampoline: Vec<Trampoline>,
+}
+
+static mut POOL: Static<InterceptorPool> = Static(MaybeUninit::zeroed());
+
 pub struct Interceptor;
 
 impl Interceptor {
+    const fn unmask_thumb2(addr: usize) -> usize {
+        addr & !1
+    }
+
+    unsafe fn place_jump(ptr: *mut u8, to: u32) {
+        unsafe {
+            (ptr as *mut u32).write_volatile(JUMP);
+            (ptr.add(4) as *mut u32).write_volatile(to);
+        }
+    }
+
+    pub unsafe fn init() {
+        unsafe {
+            POOL.init(InterceptorPool {
+                address: Vec::with_capacity(10),
+                trampoline: Vec::with_capacity(10),
+            });
+        }
+    }
+
     pub unsafe fn replace(target: usize, replacement: unsafe extern "C" fn()) -> Result<()> {
-        if target as usize & 1 == 0 {
+        if target & 1 == 0 {
             return Err(Error::UnsupportedMode);
         }
 
-        let target = (target & !1) as *mut u8;
-        unsafe {
-            ptr::write_volatile(target as *mut u32, JUMP);
-            ptr::write_volatile(target.add(4) as *mut u32, replacement as u32);
+        let mut target_ptr = Self::unmask_thumb2(target) as *mut u8;
 
-            flush_cache(target as usize);
+        let pool = unsafe { POOL.get_mut() };
+
+        let size = if target as u32 % 4 != 0 { 10 } else { 8 };
+
+        // worst case
+        let layout = unsafe { Layout::from_size_align_unchecked(64, 4) };
+
+        unsafe {
+            let code = alloc::alloc::alloc(layout);
+            let mut n_target = 0;
+            let mut n_code = 0;
+            while n_target < size {
+                let v = (target_ptr.add(n_target) as *const u16).read_unaligned();
+                if is_32bit(v) {
+                    // just copy for now
+                    ptr::copy_nonoverlapping(target_ptr.add(n_target), code.add(n_code), 4);
+                    n_target += 4;
+                    n_code += 4;
+                } else {
+                    if is_ldr(v) {
+                        let data = extract_ldr(v);
+                        let pc_aligned = (target_ptr as u32 + n_target as u32 + 4) & !3;
+                        let literal_address = pc_aligned + data.value;
+                        let value = ptr::read_volatile(literal_address as *const u32);
+                        let (movw, movt) = pack_mov_pair(data.r, value);
+                        write_thumb2_instr(code.add(n_code), movw);
+                        write_thumb2_instr(code.add(n_code + 4), movt);
+
+                        n_code += 8;
+                    } else {
+                        // just copy for now
+                        ptr::copy_nonoverlapping(target_ptr.add(n_target), code.add(n_code), 2);
+                        n_code += 2;
+                    }
+
+                    n_target += 2;
+                }
+            }
+
+            while (code as u32 + n_code as u32) % 4 != 0 {
+                // Write NOP (0xbf00) to pad alignment
+                ptr::write_volatile(code.add(n_code) as *mut u16, 0xbf00);
+                n_code += 2;
+            }
+
+            let jump_address = (target as u32 + n_target as u32) | 1;
+
+            ptr::write_volatile(code.add(n_code) as *mut u32, JUMP);
+            ptr::write_volatile(code.add(n_code + 4) as *mut u32, jump_address);
+
+            flush_cache(code as usize, 64);
+
+            pool.trampoline.push(Trampoline {
+                address: code as u32,
+                jump_address,
+            });
+        };
+
+        pool.address.push(target_ptr as u32);
+
+        unsafe {
+            if target_ptr as u32 % 4 != 0 {
+                ptr::write_volatile(target_ptr as *mut u16, NOP);
+                target_ptr = target_ptr.add(2);
+            }
+
+            Self::place_jump(target_ptr, replacement as u32 | 1);
+            flush_cache(target, 64);
         }
 
         Ok(())
+    }
+
+    pub unsafe fn revert(target: usize) -> Result<()> {
+        if target & 1 == 0 {
+            return Err(Error::UnsupportedMode);
+        }
+
+        let pool = unsafe { POOL.get_mut() };
+        let target = Self::unmask_thumb2(target);
+        let (i, trampoline) = pool
+            .address
+            .iter()
+            .position(|addr| *addr == target as _)
+            .map(|i| (i, &pool.trampoline[i]))
+            .ok_or(Error::TrampolineNotFound)?;
+
+        unsafe {
+            let size = trampoline.jump_address as usize - target;
+
+            ptr::copy_nonoverlapping(trampoline.address as *const u8, target as *mut u8, size);
+            flush_cache(target, size);
+        }
+
+        pool.address.remove(i);
+        pool.trampoline.remove(i);
+
+        Ok(())
+    }
+
+    pub unsafe fn original(target: usize) -> Option<usize> {
+        let pool = unsafe { POOL.get() };
+        let target = Self::unmask_thumb2(target);
+        pool.address
+            .iter()
+            .position(|addr| *addr == target as _)
+            .map(|i| pool.trampoline[i].address as _)
     }
 }
