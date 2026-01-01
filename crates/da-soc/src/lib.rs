e6ba49078015b40a19b4086139fe1866b4ba2d24
rva3 <rva333@protonmail.com>
2026-01-01 22:38:57 +0200

    da-soc: Nuke useless method and add docs

--- crates/da-soc/src/lib.rs
+++ crates/da-soc/src/lib.rs
@@ -3,6 +3,7 @@ pub enum SoC {
 }
 
 impl SoC {
+    /// Convert hwcode into SoC.
     pub fn try_from_hwcode(hwcode: u16) -> Option<Self> {
         match hwcode {
             0x6572 => Some(Self::MT6572),
@@ -10,35 +11,31 @@ impl SoC {
         }
     }
 
+    /// SoC hwcode.
     pub fn as_hwcode(&self) -> u16 {
         match self {
             Self::MT6572 => 0x6572,
         }
     }
 
-    /// Get DA1 SRAM address
+    /// DA1 SRAM address.
     pub fn da_sram_addr(&self) -> u32 {
         match self {
             Self::MT6572 => 0x2007000,
         }
     }
 
-    /// Get DA1 DRAM address
+    /// DA1 DRAM address.
     pub fn da_dram_addr(&self) -> u32 {
         match self {
             Self::MT6572 => 0x81e00000,
         }
     }
 
+    /// Preloader address.
     pub fn preloader_addr(&self) -> u32 {
         match self {
             Self::MT6572 => 0x2007500,
-        }
-    }
-
-    pub fn is_da1_addr_hardcoded_in_preloader(&self) -> bool {
-        match self {
-            SoC::MT6572 => true,
         }
     }
 }
