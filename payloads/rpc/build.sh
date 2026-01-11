795d1011e1b45acb03a080a2dfa952780d428ca6
rva3 <rva333@protonmail.com>
2026-01-11 23:59:07 +0200

    payloads/rpc: Better size

--- payloads/rpc/build.sh
+++ payloads/rpc/build.sh
@@ -1,2 +1,2 @@
-cargo +nightly b --release -Z build-std=core,alloc
-llvm-objcopy -O binary ../../target/armv7a-none-eabi/release/rpc ../../target/armv7a-none-eabi/release/rpc
+cargo +nightly b --profile nostd -Z build-std=core,alloc
+llvm-objcopy -O binary ../../target/armv7a-none-eabi/nostd/rpc ../../target/armv7a-none-eabi/nostd/rpc
