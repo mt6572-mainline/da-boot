7c5101a38823395fba24031f430bfbe1cc763fc1
rva3 <rva333@protonmail.com>
2026-03-05 18:39:11 +0200

    da-analyzer: Disassemble only one instruction per call

--- crates/da-analyzer/src/disasm.rs
+++ crates/da-analyzer/src/disasm.rs
@@ -1,37 +1,21 @@
-use yaxpeax_arch::{Arch, Decoder, Reader, ReaderBuilder, U8Reader};
-use yaxpeax_arm::armv7::{ARMv7, DecodeError, InstDecoder};
+use yaxpeax_arch::{Arch, Decoder, ReaderBuilder};
+use yaxpeax_arm::armv7::{ARMv7, InstDecoder};
 
-use crate::Code;
+use crate::{Code, Result};
 
-fn disassemble(decoder: InstDecoder, data: &[u8]) -> Vec<Code> {
+fn disassemble_oneshot(decoder: InstDecoder, data: &[u8]) -> Result<Code> {
     let mut reader =
         ReaderBuilder::<<ARMv7 as Arch>::Address, <ARMv7 as Arch>::Word>::read_from(data);
-
-    let mut vec = Vec::with_capacity(10 * 1024);
-
-    loop {
-        let address =
-            <U8Reader<'_> as Reader<u32, <ARMv7 as Arch>::Word>>::total_offset(&mut reader);
-        let decode_res = decoder.decode(&mut reader);
-
-        match decode_res {
-            Ok(inst) => {
-                vec.push(Code::new(inst, address as usize));
-            }
-            Err(e) => match e {
-                DecodeError::ExhaustedInput => break,
-                _ => (), // Decode errors are not fatal, we don't know if it's data disassembled as junk or incomplete decoder
-            },
-        }
-    }
-
-    vec
+    decoder
+        .decode(&mut reader)
+        .map(|inst| Code::new(inst, 0))
+        .map_err(Into::into)
 }
 
-pub fn disassemble_thumb(data: &[u8]) -> Vec<Code> {
-    disassemble(InstDecoder::armv7_thumb(), data)
+pub fn disassemble_thumb_oneshot(data: &[u8]) -> Result<Code> {
+    disassemble_oneshot(InstDecoder::armv7_thumb(), data)
 }
 
-pub fn disassemble_arm(data: &[u8]) -> Vec<Code> {
-    disassemble(InstDecoder::armv7(), data)
+pub fn disassemble_arm_oneshot(data: &[u8]) -> Result<Code> {
+    disassemble_oneshot(InstDecoder::armv7(), data)
 }
