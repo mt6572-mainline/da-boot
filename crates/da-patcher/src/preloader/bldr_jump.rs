947abdb68a29e42df89ef3d22abda021d99b1b80
rva3 <rva333@protonmail.com>
2026-07-25 21:24:43 +0300

    da-patcher: rework bldr_jump search for some edge cases like 6582

--- crates/da-patcher/src/preloader/bldr_jump.rs
+++ crates/da-patcher/src/preloader/bldr_jump.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Result};
-use kaiko::Analyzer;
+use kaiko::{Analyzer, yaxpeax_arm::armv7::Opcode};
 
 use crate::{Extract, extractor};
 
@@ -8,36 +8,35 @@ impl Extract for BldrJump<'_> {
     type Value = (u32, u32);
 
     fn extract(&self) -> Result<Self::Value> {
-        const JUMP_DA: &str = "%s usbdl_jump_da: %x\n";
-
         let f = self
             .analyzer
-            .fn_by_str(JUMP_DA)
+            .fn_by_str("%s usbdl_jump_da: %x\n")
             .context("string not found")?;
-        let block = self
-            .analyzer
-            .block_by_str(JUMP_DA)
-            .context("string not found")?;
-        let idx = f
-            .blocks()
-            .position(|b| b == block)
-            .expect("BUG: fn must contain the block");
 
-        // bldr_jump block is the next one
-        let next = f
+        // we need a block with at least 2 literal loads and 4 BLX calls
+        let block = f
             .blocks()
-            .nth(idx + 1)
-            .context("unexpected end of function")?;
+            .find(|b| {
+                b.code()
+                    .filter(|c| c.instruction().opcode == Opcode::LDR)
+                    .count()
+                    >= 2
+                    && b.code()
+                        .filter(|c| c.instruction().opcode == Opcode::BLX)
+                        .count()
+                        >= 4
+            })
+            .context("bldr_jump block not found: need at least 2 literal loads and 4 BLX calls")?;
 
-        let (_, bldr_jump) = next
+        let (_, bldr_jump) = block
             .fn_calls()
             .last()
             .context("calls in the bldr_jump block must exist")?;
 
         // what a hack.
-        let da_addr = next
+        let da_addr = block
             .data_refs()
-            .find_map(|(_, v)| ((v & !0xffff) == v).then_some(v))
+            .find_map(|(_, v)| ((v & !0xfff) == v).then_some(v))
             .context("DA addr not found")?;
         Ok((bldr_jump, da_addr))
     }
